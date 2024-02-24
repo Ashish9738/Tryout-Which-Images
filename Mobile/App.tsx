c8152d7d160109d41967eb24b78fdae98932f6df
Ashish Yadav <ayadav9738@gmail.com>
2024-02-25 01:10:25 +0530

    modified:   Mobile/App.tsx 	new file:   Mobile/src/components/Feedback/Feedback.tsx

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import TestModelScreen from './src/components/DropDown/screens/TestModelScreen';
+import Feedback from './src/components/Feedback/Feedback';
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <TestModelScreen />
+      <Feedback />
     </View>
   );
 };
