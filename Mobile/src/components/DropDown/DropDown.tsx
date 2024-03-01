bedfe6f68e5ff435f9e93f7b06f24f8877e323ac
Ashish Yadav <ayadav9738@gmail.com>
2024-03-01 18:50:55 +0530

    modified:   Mobile/src/components/DropDown/DropDown.tsx

--- Mobile/src/components/DropDown/DropDown.tsx
+++ Mobile/src/components/DropDown/DropDown.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import {useState, useEffect} from 'react';
+import {View, Button, Text, StyleSheet} from 'react-native';
 import TestModelScreen from './screens/TestModelScreen';
-import { fetchModels } from './utils/Api';
+import {fetchModels} from './utils/Api';
 
 const DropDown: React.FC = () => {
   const [pickerVisible, setPickerVisible] = useState<boolean>(false);
   const [selectedModel, setSelectedModel] = useState<any>(null);
   const [models, setModels] = useState<any[]>([]);
   const [error, setError] = useState<string | null>(null);
-  
+
   const fetchInitialModelData = async () => {
     try {
       const data = await fetchModels();
@@ -37,21 +37,17 @@ const DropDown: React.FC = () => {
 
   useEffect(() => {
     fetchInitialModelData();
+    const fetchDataInterval = setInterval(fetchInitialModelData, 60000);
+    return () => clearInterval(fetchDataInterval);
   }, []);
 
   return (
     <View>
       <View style={styles.buttonContainer}>
-        <Button
-          title="Select Model"
-          onPress={togglePicker}
-        />
+        <Button title="Select Model" onPress={togglePicker} />
       </View>
       {pickerVisible && (
-        <TestModelScreen
-          onSelect={handleSelectModel}
-          models={models}
-        />
+        <TestModelScreen onSelect={handleSelectModel} models={models} />
       )}
 
       {/* To display the Model which is been selected */}
